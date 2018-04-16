d76f80e1b2a8fe902b76795aacf7d49a9f3982ef
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-16 16:35:36 -0500

    added get @/api

--- server/server.js
+++ server/server.js
@@ -3,18 +3,39 @@ const express = require('express'),
 
 
 const { mongoose } = require('./db/mongoose')
-const { AirportArrivals } = require('./models/AirportArrival')
+const { AirportArrival } = require('./models/AirportArrival')
 
 const app = express(),
       port = 3333
 
 app.use(bodyParser.json())
 
-app.post('/arrivals', (req, res) => {
+app.post('/api', (req, res) => {
+	const arrival = new AirportArrival({
+		city: req.body.city,
+		country: req.body.country,
+		airport: req.body.airport,
+		date: req.body.date
+	})
+	arrival.save()
+		.then(doc => {
+			res.send(doc)
+		}, e => {
+			res.status(400).send(e)
+		})
+})
 
-	res.send(req.body)
+app.get('/api', (req, res) => {
+	AirportArrival.find()
+		.then(arrival => {
+			res.send({ arrival })
+		}, e => {
+			res.status(400).send(e)
+		})
 })
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 })
+
+module.exports = { app }
