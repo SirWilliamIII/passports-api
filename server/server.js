f04040dd17b2a137dd28791ef15e04cdbed2bc92
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-16 15:38:11 -0500

    created post endpoint

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
-const { mongoose } = require('./db/mongoose')
+const express = require('express'),
+      bodyParser = require('body-parser')
 
+
+const { mongoose } = require('./db/mongoose')
+const { AirportArrivals } = require('./models/AirportArrival')
+
+const app = express(),
+      port = 3333
+
+app.use(bodyParser.json())
+
+app.post('/arrivals', (req, res) => {
+
+	res.send(req.body)
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
